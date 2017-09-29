0d3e28fcf75d181a82ad3b161b37ade9d54e6ee6
teshi24 <nadja.stadi@gmail.com>
2017-09-29 13:55:01 +0200

    metal detector: this app should work now

--- MetallDetector/app/src/main/java/ch/appquest/brudinne/metalldetector/MainActivity.java
+++ MetallDetector/app/src/main/java/ch/appquest/brudinne/metalldetector/MainActivity.java
@@ -2,20 +2,21 @@ package ch.appquest.brudinne.metalldetector;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.FloatMath;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends Activity implements SensorEventListener {
@@ -23,15 +24,18 @@ public class MainActivity extends Activity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor metallDetector;
     private ProgressBar metallic;
-
+    private TextView status;
     private Menu mainMenu;
-    private String menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         metallic = (ProgressBar) findViewById(R.id.metallic);
+        status = (TextView) findViewById(R.id.status);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            metallic.setMin(10);
+        }
         mainMenu = (Menu) findViewById(R.id.menu);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
@@ -53,13 +57,22 @@ public class MainActivity extends Activity implements SensorEventListener {
         float[] met = sensorEvent.values;
         double betrag = Math.sqrt(met[0] * met[0] + met[1] * met[1] + met[2] * met[2]);
         metallic.setProgress((int)betrag);
+        if(betrag < 30){
+            status.setText("You are far away of your treasure.");
+        }else if(betrag < 60){
+            status.setText("You are getting nearer to your treasure.");
+        }
+        else if(betrag < 85){
+            status.setText("You have almost found your treasure!");
+        }else{
+            status.setText("Treasure found! You made it!");
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-
+        // not important
     }
-
 
 
     private static final int SCAN_QR_CODE_REQUEST_CODE = 0;
@@ -67,8 +80,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuItem menuItem = menu.add("Log");
-        menuItem.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 Intent intent = new Intent("com.google.zxing.client.android.SCAN");
@@ -77,7 +90,6 @@ public class MainActivity extends Activity implements SensorEventListener {
                 return false;
             }
         });
-
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -86,7 +98,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         if (requestCode == SCAN_QR_CODE_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 log(intent.getStringExtra("SCAN_RESULT"));
-                // Weiterverarbeitung..
             }
         }
     }
@@ -99,17 +110,15 @@ public class MainActivity extends Activity implements SensorEventListener {
             Toast.makeText(this, "Logbook App not Installed", Toast.LENGTH_LONG).show();
             return;
         }
-
-        // Achtung, je nach App wird etwas anderes eingetragen
-        String logmessage = ...
-        intent.putExtra("ch.appquest.logmessage", logmessage);
-
+        try{
+            //to test log
+            //log.put("task", "TEST");
+            //log.put("solution", "482ae9");
+            log.put("task", "Metalldetektor");
+            log.put("solution", qrCode);
+        }catch (JSONException e){
+        }
+        intent.putExtra("ch.appquest.logmessage", log.toString());
         startActivity(intent);
     }
-
-    public void log(String)
-    JSONObject log = JSONObject();
-    log.put
-    log.put
-    intent.putExtra(log.toString())
 }
