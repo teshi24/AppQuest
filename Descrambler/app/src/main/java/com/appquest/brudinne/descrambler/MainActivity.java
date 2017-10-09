e3f218198a7428c0166051eea53b6da1681a8e9f
teshi24 <nadja.stadi@gmail.com>
2017-10-09 06:40:06 +0200

    MetalDetector finished, Descrambler started

---
+++ Descrambler/app/src/main/java/com/appquest/brudinne/descrambler/MainActivity.java
@@ -0,0 +1,176 @@
+package com.appquest.brudinne.descrambler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+//import java.util.Date;
+
+public class MainActivity extends Activity {
+
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    ImageView mImageView;
+    String mCurrentPhotoPath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            //Uri eingelesen ohne extras
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                //...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.appquest.brudinne.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+
+            String mCurrentPhotoPath;
+
+            private File createImageFile() throws IOException {
+                // Create an image file name
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "_";
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                File image = File.createTempFile(
+                        imageFileName,  /* prefix */
+                        ".jpg",         /* suffix */
+                        storageDir      /* directory */
+                );
+
+                // Save a file: path for use with ACTION_VIEW intents
+                mCurrentPhotoPath = image.getAbsolutePath();
+                return image;
+            }
+        }
+
+        private Bitmap readImageFile(Uri imageUri) {
+            File file = new File(imageUri.getPath());
+            InputStream is = null;
+            try {
+                is = new FileInputStream(file);
+                Bitmap bitmap = BitmapFactory.decodeStream(is);
+                return bitmap;
+            } catch (FileNotFoundException e) {
+                Log.e("DECODER", "Could not find image file", e);
+                return null;
+            } finally {
+                if(is != null) {
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
+
+    private Bitmap applyFilter(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int[] data = new int[width * height];
+
+        bitmap.getPixels(data, 0, width, 0, 0, width, height);
+
+        // Hier können die Pixel im data-array bearbeitet und
+        // anschliessend damit ein neues Bitmap erstellt werden
+
+        return Bitmap.createBitmap(data, width, height, Bitmap.Config.ARGB_8888);
+    }
+
+    private void log(String qrCode) {
+        Intent intent = new Intent("ch.appquest.intent.LOG");
+        JSONObject log = new JSONObject();
+
+        if(checkInstalled(intent, "Logbook")) {
+            try {
+                //to test log
+                //log.put("task", "TEST");
+                //log.put("solution", "482ae9");
+                log.put("task", "Dechiffrierer");
+                log.put("solution", qrCode);
+            } catch (JSONException e) {
+            }
+            intent.putExtra("ch.appquest.logmessage", log.toString());
+            startActivity(intent);
+        }
+    }
+
+    private boolean checkInstalled(Intent intent, String appName){
+        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+            Toast.makeText(this, appName + " App not Installed", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+}
