43a16a96e9d4342aec578b1e37475f75d24aad96
teshi24 <nadja.stadi@gmail.com>
2017-10-09 08:27:21 +0200

    zip test

--- Descrambler/app/src/main/java/com/appquest/brudinne/descrambler/MainActivity.java
+++ Descrambler/app/src/main/java/com/appquest/brudinne/descrambler/MainActivity.java
@@ -48,7 +48,6 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 //import java.util.Date;
 
 public class MainActivity extends Activity {
