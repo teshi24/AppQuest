2751edf60551b9d89fa74d2d4ed65f3a3d12c0d8
matalie <natalie.stalder1@gmail.com>
2017-10-16 08:58:04 +0200

    first code parts memory

--- Memory/app/src/main/java/ch/appquest/brudinne/memory/MainActivity.java
+++ Memory/app/src/main/java/ch/appquest/brudinne/memory/MainActivity.java
@@ -1,7 +1,16 @@
 package ch.appquest.brudinne.memory;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,109 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takeQrCodePicture() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(MyCaptureActivity.class);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setOrientationLocked(false);
+        integrator.addExtra(Intents.Scan.BARCODE_IMAGE_ENABLED, true);
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IntentIntegrator.REQUEST_CODE
+                && resultCode == RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            String path = extras.getString(
+                    Intents.Scan.RESULT_BARCODE_IMAGE_PATH);
+
+            // Ein Bitmap zur Darstellung erhalten wir so:
+            // Bitmap bmp = BitmapFactory.decodeFile(path)
+
+            String code = extras.getString(
+                    Intents.Scan.RESULT);
+        }
+    }
+
+    RecyclerView rv = (RecyclerView)findViewById(R.id.recyclerView);
+    GridLayoutManager gridLayoutManager = new GridLayoutManager(this /* the activity */, 2);
+    rv.setLayoutManager(gridLayoutManager);
+
+    // log message handling
+    // --------------------
+
+    /**
+     * prepare log button with QR-Code Intent
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.add("Log");
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                if(checkInstalled(intent, "Zxing Barcode Scanner")){
+                    startActivityForResult(intent, SCAN_QR_CODE_REQUEST_CODE);
+                }
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * get result from QR-Code Intent
+     * @param requestCode
+     * @param resultCode
+     * @param intent
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == SCAN_QR_CODE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                log(intent.getStringExtra("SCAN_RESULT"));
+            }
+        }
+    }
+
+    /**
+     * send log message to Logbook
+     * @param qrCode
+     */
+    private void log(String qrCode) {
+        Intent intent = new Intent("ch.appquest.intent.LOG");
+        JSONObject log = new JSONObject();
+
+        if(checkInstalled(intent, "Logbook")) {
+            try {
+                log.put("task", "Metalldetektor");
+                log.put("solution", qrCode);
+            } catch (JSONException e) {
+            }
+            intent.putExtra("ch.appquest.logmessage", log.toString());
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * checks if an application is installed
+     * @param intent
+     * @param appName
+     * @return
+     */
+    private boolean checkInstalled(Intent intent, String appName){
+        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+            Toast.makeText(this, appName + " App not Installed", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
