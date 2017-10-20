32b89a5f3bc98b26add34ef3a2923e1e1308089f
matalie <natalie.stalder1@gmail.com>
2017-10-20 09:02:15 +0200

    speicher

--- Memory/app/src/main/java/ch/appquest/brudinne/memory/MainActivity.java
+++ Memory/app/src/main/java/ch/appquest/brudinne/memory/MainActivity.java
@@ -1,6 +1,9 @@
 package ch.appquest.brudinne.memory;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +15,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.zxing.client.android.Intents;
@@ -23,6 +27,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -36,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<View> list;
     private JSONArray jsonArray;
     private String currentPhotoPath;
+    public static  final String FILE_NAME = "MyJsonFile";
+    public static final String STRING_NAME = "JSON";
+    private File PICTURE_FILE;
+    private String PICTURE_NAME;
+    // TODO Natalie: change string to picture bitmap save variable
+    //private String PICTURE_NAME = "hello world!";
 
     /**
      * on create method
@@ -44,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get saved instance of JSON-Array with Name-Pairs and their links of pictures
+        SharedPreferences settings = getSharedPreferences(FILE_NAME, 0);
+        String memoryPairs = settings.getString("silentMode", null);
+
         setContentView(R.layout.activity_main);
         rv = (RecyclerView)findViewById(R.id.recyclerView);
         //adapter = new CustomAdapter(getApplicationContext(),ApplicationState.getGridElements());
@@ -69,6 +86,21 @@ public class MainActivity extends AppCompatActivity {
                 takeQrCodePicture();
             }
         });*/
+    }
+
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        // Saving JSON-Array before app gets hidden
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(FILE_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("JSON", mSilentMode);
+
+        // Committing the edits
+        editor.commit();
     }
 
     public void onStart(){
@@ -115,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
             String code = extras.getString(
                     Intents.Scan.RESULT);
             try {
-                savePicture(BitmapFactory.decodeFile(path), code, 1);
+                savePicture(BitmapFactory.decodeFile(path), code /*, 1*/);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -134,11 +166,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void savePicture(Bitmap picture, String word, int index) throws IOException {
+    private String savePicture(Bitmap picture, String word /*, int index*/) throws IOException {
+        // Create Filename for picture
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String tempFilePath = Environment.getExternalStorageDirectory() + "/" + index + "_" + word + "_" + timeStamp +".jpg";
+        PICTURE_NAME = Environment.getExternalStorageDirectory() + "/" + word + "_" + timeStamp +".jpg";
 
-        File tempFile = new File(tempFilePath);
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        PICTURE_FILE = new File(directory, PICTURE_NAME);
+
+        FileOutputStream fos = null;
+        try{
+            fos = new FileOutputStream(PICTURE_FILE);
+            picture.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return directory.getAbsolutePath();
+        }
+        /*File tempFile = new File(tempFilePath);
         if (!tempFile.exists()) {
             if (!tempFile.getParentFile().exists()) {
                 tempFile.getParentFile().mkdirs();
@@ -158,8 +209,25 @@ public class MainActivity extends AppCompatActivity {
         bos.close();
 
         picture.recycle();
+        */
 
         //return tempFilePath;
+    }
+
+    private void loadImageFromStorage(String path)
+    {
+
+        try {
+            File f = new File(path, "profile.jpg");
+            Bitmap b = BitmapFactory.decodeStream(new FileInputStream(f));
+            ImageView img=(ImageView)findViewById(R.id.image);
+            img.setImageBitmap(b);
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
 
