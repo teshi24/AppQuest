e36eafdb62d4a02e4a883398845e2c71db2a814e
teshi24 <nadja.stadi@gmail.com>
2017-11-10 16:15:42 +0100

    before merge

--- TreasureHunt/app/src/main/java/com/appquest/brudinne/treasurehunt/MainActivity.java
+++ TreasureHunt/app/src/main/java/com/appquest/brudinne/treasurehunt/MainActivity.java
@@ -1,8 +1,17 @@
 package com.appquest.brudinne.treasurehunt;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import org.osmdroid.api.IMapController;
@@ -11,9 +20,13 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
     private MapView map;
     private IMapController controller;
+    private LocationManager locationManager;
+    GeoPoint startPoint;
+    private String provider;
+    private int latitute, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,36 @@ public class MainActivity extends AppCompatActivity {
         //important! set your user agent to prevent getting banned from the osm servers
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         setContentView(R.layout.activity_main);
+
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return  ;
+        }
+
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        // check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to
+        // go to the settings
+        if (!enabled) {
+            // todo: change to dialog and do in background if possible (stichwort AlarmDialog)
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria,false);
+        Location location = null;
+
+        location = locationManager.getLastKnownLocation(provider);
+
+        if(location != null){
+            onLocationChanged(location);
+        }else{
+            //todo: error because of latitudes..
+        }
 
         map = (MapView)findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
@@ -37,11 +80,19 @@ public class MainActivity extends AppCompatActivity {
         //todo: something like this:
         // move map on default view point
         // todo: this is paris, should be current position via GPS
-        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        startPoint = new GeoPoint(48.8583, 2.2944);
+        //startPoint = new GeoPoint(location);
         controller.setCenter(startPoint);
 
     }
 
+    @Override
+    public void onPause(){
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    @Override
     public void onResume(){
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
@@ -49,5 +100,28 @@ public class MainActivity extends AppCompatActivity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
+        //locationManager.requestLocationUpdates(provider,400,1,this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        startPoint.setLatitude(location.getLatitude());
+        startPoint.setLongitude(location.getLongitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        //todo: iwas auto-generated method stub http://www.vogella.com/tutorials/AndroidLocationAPI/article.html
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
