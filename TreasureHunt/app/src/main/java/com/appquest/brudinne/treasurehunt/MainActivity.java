862fee085242130339a0a17ff417e302ff6f4122
matalie <natalie.stalder1@gmail.com>
2017-11-10 15:43:27 +0100

    icons, jsonarray, log

--- TreasureHunt/app/src/main/java/com/appquest/brudinne/treasurehunt/MainActivity.java
+++ TreasureHunt/app/src/main/java/com/appquest/brudinne/treasurehunt/MainActivity.java
@@ -1,19 +1,35 @@
 package com.appquest.brudinne.treasurehunt;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 
 public class MainActivity extends AppCompatActivity {
     private MapView map;
     private IMapController controller;
+    Drawable marker  = getResources().getDrawable(android.R.drawable.star_big_on);
+    ResourceProxy resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+
+    private JSONArray latLongList = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,4 +66,81 @@ public class MainActivity extends AppCompatActivity {
         //Configuration.getInstance().save(this, prefs);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
     }
+
+    public void addSignToMap(){
+
+    }
+
+    public void addToJsonArray(int latitude, int longitude){
+        JSONObject object = new JSONObject();
+        try {
+            object.put("lat", Integer.toString(latitude));
+            object.put("lon", Integer.toString(longitude));
+            latLongList.put(object);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    // log message handling
+    // --------------------
+
+    /**
+     * prepare log
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.add("Log");
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                log();
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * send log message to Logbook
+     */
+    private void log() {
+        Intent intent   = new Intent("ch.appquest.intent.LOG");
+        JSONObject log  = new JSONObject();
+        if (checkInstalled(intent, "Logbook")) {
+            try {
+                if(latLongList != null && latLongList.length() > 0) {
+                    log.put("task", "Schatzkarte");
+                    log.put("points", latLongList);
+                } else {
+                    Toast.makeText(this, "No matches to log.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            } catch (JSONException e) {
+            }
+            intent.putExtra("ch.appquest.logmessage", log.toString());
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * checks, if an app is installed
+     *
+     * @param intent
+     * @param appName
+     * @return
+     */
+    private boolean checkInstalled(Intent intent, String appName) {
+        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+            Toast.makeText(this, appName + " App not Installed", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
 }
+
