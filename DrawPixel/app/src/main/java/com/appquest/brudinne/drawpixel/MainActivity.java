927fe93414d17dd95e15525d68db282ce4fd80f0
teshi24 <nadja.stadi@gmail.com>
2017-11-25 16:25:20 +0100

    new project

---
+++ DrawPixel/app/src/main/java/com/appquest/brudinne/drawpixel/MainActivity.java
@@ -0,0 +1,166 @@
+package com.appquest.brudinne.drawpixel;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawingView;
+    private ImageButton currentBrush;
+
+    public void eraseClicked(View view) {
+        if (view != currentBrush) {
+            ImageButton imgView = (ImageButton) view;
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.selected));
+            currentBrush.setImageDrawable(null);
+            currentBrush = (ImageButton) view;
+        }
+        drawingView.setErase(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawingView = (DrawingView) findViewById(R.id.drawing);
+
+        currentBrush = (ImageButton) findViewById(R.id.defaultColor);
+        currentBrush.setImageDrawable(getResources().getDrawable(R.drawable.selected));
+        String color = currentBrush.getTag().toString();
+        drawingView.setColor(color);
+    }
+
+    private void onCreateNewDrawingAction() {
+        AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
+        newDialog.setTitle("New Drawing");
+        newDialog.setMessage("Start a new drawing?");
+        newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                drawingView.startNew();
+                dialog.dismiss();
+            }
+        });
+        newDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        newDialog.show();
+    }
+
+    public void paintClicked(View view) {
+        if (view != currentBrush) {
+            ImageButton imgView = (ImageButton) view;
+            String color = view.getTag().toString();
+            drawingView.setColor(color);
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.selected));
+            currentBrush.setImageDrawable(null);
+            currentBrush = (ImageButton) view;
+        }
+        drawingView.setErase(false);
+    }
+
+    private void onLogAction() {
+        // TODO
+    }
+
+    // log message handling
+    // --------------------
+
+    /**
+     * prepare log
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem menuItem = menu.add("New");
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem arg0) {
+                onCreateNewDrawingAction();
+                return true;
+            }
+        });
+
+        menuItem = menu.add("Log");
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                log();
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    /**
+     * send log message to Logbook
+     */
+    private void log() {
+        //todo: log entsprechend anpassen
+        Intent intent = new Intent("ch.appquest.intent.LOG");
+        JSONObject log = new JSONObject();
+        if (checkInstalled(intent, "Logbook")) {
+            try {
+                //makeJSONArray();
+                JSONArray savedArray = new JSONArray();
+                if (savedArray != null && savedArray.length() > 0) {
+                    log.put("task", "Pixelmaler");
+                    log.put("points", savedArray);
+                } else {
+                    Toast.makeText(this, "No matches to log.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            } catch (JSONException e) {
+                return;
+            }
+            intent.putExtra("ch.appquest.logmessage", log.toString());
+            startActivity(intent);
+        }
+    }
+    /* todo: how to log:
+    {
+      "task": "Pixelmaler",
+      "pixels": [
+        {"y":"0","x":"0","color":"#ffea00FF"},
+        {"y":"0","x":"1","color":"#1364b7FF"},
+        {"y":"0","x":"2","color":"#1364b7FF"},
+        {"y":"0","x":"3","color":"#ffea00FF"},
+        ...
+        ]
+    }
+     */
+
+    /**
+     * checks, if an app is installed
+     *
+     * @param intent
+     * @param appName
+     * @return
+     */
+    private boolean checkInstalled(Intent intent, String appName) {
+        if (getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).isEmpty()) {
+            Toast.makeText(this, appName + " App not Installed", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+}
